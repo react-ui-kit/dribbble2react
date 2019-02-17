bbfea0a6d0f36f70f505a89b99a46c952740e753
Hetmann W. Iohan <contact@react-ui-kit.com>
2019-02-17 19:22:23 +0200

    Added parking app

---
+++ parking-app/theme.js
@@ -0,0 +1,18 @@
+const COLORS = {
+  red: '#D83C54',
+  gray: '#7D818A',
+  black: '#3D4448',
+  white: '#FFFFFF',
+  overlay: '#C1BEC0',
+};
+
+const SIZES = {
+  base: 12,
+  icon: 16,
+  font: 16,
+}
+
+export {
+  COLORS,
+  SIZES,
+}
