c5e166c8b3e66c16521844a3e480ae031ab06a60
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-05-05 21:50:31 +0100

    Use one-line ternary
    Add ternary and function component.

--- iBlood/components/Block.js
+++ iBlood/components/Block.js
@@ -3,49 +3,46 @@ import { StyleSheet, View } from 'react-native'
 
 import * as theme from '../theme';
 
-export default class Block extends Component {
-  render() {
-    const {
-      flex,
-      row,
-      column,
-      center,
-      middle,
-      left,
-      right,
-      card,
-      shadow,
-      color,
-      space,
-      style,
-      children,
-      ...props
-    } = this.props;
+const Block = props => {
+  const {
+    flex,
+    row,
+    column,
+    center,
+    middle,
+    left,
+    right,
+    card,
+    shadow,
+    color,
+    space,
+    style,
+    children,
+    ...props
+  } = props;
 
-    const blockStyles = [
-      styles.block,
-      flex && { flex },
-      flex === false && { flex: 0 }, // reset / disable flex
-      row && styles.row,
-      column && styles.column,
-      center && styles.center,
-      middle && styles.middle,
-      left && styles.left,
-      right && styles.right,
-      card && styles.card,
-      shadow && styles.shadow,
-      space && { justifyContent: `space-${space}` },
-      color && styles[color], // predefined styles colors for backgroundColor
-      color && !styles[color] && { backgroundColor: color }, // custom backgroundColor
-      style, // rewrite predefined styles
-    ];
+  const blockStyles = [
+    styles.block,
+    flex ? { flex } : { flex: 0 },
+    row && styles.row,
+    column && styles.column,
+    center && styles.center,
+    middle && styles.middle,
+    left && styles.left,
+    right && styles.right,
+    card && styles.card,
+    shadow && styles.shadow,
+    space && { justifyContent: `space-${space}` },
+    color && styles[color], // predefined styles colors for backgroundColor
+    color && !styles[color] && { backgroundColor: color }, // custom backgroundColor
+    style, // rewrite predefined styles
+  ];
 
-    return (
-      <View style={blockStyles} {...props}>
-        {children}
-      </View>
-    )
-  }
+  return (
+    <View style={blockStyles} {...props}>
+      {children}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
