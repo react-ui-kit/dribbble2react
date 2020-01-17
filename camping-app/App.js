ec511cee410f06ee926fce98919f5fe35dd9a3e8
Hetmann W. Iohan <contact@react-ui-kit.com>
2020-01-17 08:46:10 +0200

    Camping App: Updated to Expo SDK 36 (#54)
    * Plant App - Updated to Expo SDK 36
    
    * Camping App: Updated to Expo SDK 36

--- camping-app/App.js
+++ camping-app/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Icon } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import AppNavigator from './navigation/AppNavigator';
+import React from "react";
+import { Provider } from "react-redux";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { AppLoading } from "expo";
+import AppNavigator from "./navigation/AppNavigator";
 
-import store from './modules';
+import store from "./modules";
 
 export default class App extends React.Component {
   state = {
-    isLoadingComplete: false,
+    isLoadingComplete: false
   };
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
       return (
         <AppLoading
-          startAsync={this._loadResourcesAsync}
+          startAsync={() => {}}
           onError={this._handleLoadingError}
           onFinish={this._handleFinishLoading}
         />
@@ -26,29 +24,13 @@ export default class App extends React.Component {
       return (
         <Provider store={store}>
           <View style={styles.container}>
-            {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+            {Platform.OS === "ios" && <StatusBar barStyle="default" />}
             <AppNavigator />
           </View>
         </Provider>
       );
     }
   }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
 
   _handleLoadingError = error => {
     // In this case, you might want to report the error to your error
@@ -64,6 +46,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
--- camping-app/constants/Colors.js
+++ camping-app/constants/Colors.js
@@ -1,14 +1,14 @@
-const tintColor = '#2f95dc';
+const tintColor = "#2f95dc";
 
 export default {
   tintColor,
-  tabIconDefault: '#ccc',
+  tabIconDefault: "#ccc",
   tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
+  tabBar: "#fefefe",
+  errorBackground: "red",
+  errorText: "#fff",
+  warningBackground: "#EAEB5E",
+  warningText: "#666804",
   noticeBackground: tintColor,
-  noticeText: '#fff',
+  noticeText: "#fff"
 };
